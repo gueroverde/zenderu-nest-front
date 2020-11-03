47213d8579ab68d218e32ef8061ee56595115099
Joshua Martinez <skynessj@gmail.com>
2020-11-03 01:48:08 -0400

    perfect minible, clean

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,9 +1,5 @@
 module.exports = {
-  extends: [
-    'stylelint-config-standard',
-    'stylelint-config-prettier'
-  ],
-  // add your custom config here
-  // https://stylelint.io/user-guide/configuration
-  rules: {}
+  extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
+  ignoreFiles: ['assets/**/*.scss'],
+  rules: {},
 }
